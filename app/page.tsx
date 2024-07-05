dec04efa1e219198656f1b49a8e6ecf0dee8691e
Dominik - Arbeit <led1af@bosch.com>
2024-07-05 09:08:41 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -46,6 +46,7 @@ export default function Home() {
             Kostenlose Beratung
           </button>
           <Image
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
             src="/highlight_white.svg"
             alt="highlight"
             width={800}
