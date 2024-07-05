9cf302ec14ec72e3f5b1500cf5ce110c9b736b2a
Dominik - Arbeit <led1af@bosch.com>
2024-07-05 09:04:54 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const monte = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="w-full h-full">
+      <body className={monte.className + " w-full h-full bg-brand-dark"}>
+        {children}
+      </body>
     </html>
   );
 }
