8dae5eb850e4b75cfcf0003ae16e8f1ccd5933c9
Dominik - Arbeit <led1af@bosch.com>
2024-07-09 11:07:32 +0200

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "brand-dark": "#222222",
+        "brand-prim": "#FF800B",
+        "brand-sec": "#FAFF00",
+        "brand-whiteish": "#D9D9D9",
       },
     },
   },
